ba1f786adc236a85dc592c0bed98ad783b62cbf6
kacperg1999 <kacpergrzesiak1999@gmail.com>
2021-04-09 11:26:32 +0200

    dodanie i pozycjonowanie przyciskow

--- opencv_Template/opencv_Template/functions.cpp
+++ opencv_Template/opencv_Template/functions.cpp
@@ -4,24 +4,59 @@
 sfml_objects::sfml_objects()
 {
 	this->window = new sf::RenderWindow(sf::VideoMode(1920, 1080), "Window");
-	this->green_button_x = 0;
-	this->green_button_y = 0;
+
+}
+
+// Initializations
+void sfml_objects::init_button_size(int percentege_size)
+{
+	//coordinates for green button
+	this->green_button_x = 1696;
+	this->green_button_y = 856;
+
+	//coordinates for red button
+	this->red_button_x = 100;
+	this->red_button_y = 856;
+
+	//coordinates for blue button
+	this->blue_button_x = 0;
+	this->blue_button_y = 0;
 }
 
 //Displaying objects
-void sfml_objects::display_next_button(int pos_x, int pos_y)
+void sfml_objects::display_texture(int pos_x, int pos_y, std::string file_path, float scale_x, float scale_y)
 {
-	sf::Texture green_button_;
-	if (!green_button_.loadFromFile("resources/NBA.jpg"))
+	sf::Texture texture_;
+	if (!texture_.loadFromFile("resources/" + file_path))
 	{
 		std::cerr << "Could not load texture" << std::endl;
 		exit(1);
 	}
-	sf::Sprite sGreen_button;
-	sGreen_button.setTexture(green_button_);
-	sGreen_button.setPosition(pos_x, pos_y);
+	sf::Sprite texture;
+	texture.setTexture(texture_);
+	texture.setPosition(pos_x, pos_y);
+	texture.setScale(scale_x, scale_y);
 
-	this->window->draw(sGreen_button);
+
+	this->window->draw(texture);
+}
+
+void sfml_objects::display_text(int pos_x, int pos_y, std::string text)
+{
+	sf::Font font_;
+	if (!font_.loadFromFile("resources/mermaid/Mermaid1001.ttf"))
+	{
+		std::cerr << "Could not load font" << std::endl;
+		exit(1);
+	}
+	sf::Text text_;
+	text_.setFillColor(sf::Color::Black);
+	text_.setPosition(pos_x, pos_y);
+	text_.setFont(font_);
+	text_.setString(text);
+	text_.setCharacterSize(40);
+	
+	this->window->draw(text_);
 }
 
 //Accessors
@@ -45,9 +80,13 @@ void sfml_objects::update()
 	this->pollEvents();
 }
 
+
 void sfml_objects::render()
 {
 	this->window->clear(sf::Color(255,255,255,255));
-	this->display_next_button(this->green_button_x, this->green_button_y);
+	this->display_texture(this->green_button_x, this->green_button_y, "green_circle.png", 0.3, 0.3);
+	this->display_texture(this->red_button_x, this->red_button_y, "red_circle.png", 0.3, 0.3);
+	//this->display_texture(this->blue_button_x, this->blue_button_y, "blue_circle.png", 0.3, 0.3);
+	this->display_text(this->green_button_x, this->green_button_y, "Continue");
 	this->window->display();
 }
--- opencv_Template/opencv_Template/header.h
+++ opencv_Template/opencv_Template/header.h
@@ -3,20 +3,36 @@
 #include "opencv2/opencv.hpp"
 #include <SFML/Graphics.hpp>
 
+
 class sfml_objects
 {
 public:
-	sf::Sprite green_button;
+	//coordinates for green button
 	int green_button_x;
 	int green_button_y;
+
+	//coordinates for red button
+	int red_button_x;
+	int red_button_y;
+
+	//coordinates for blue button
+	int blue_button_x;
+	int blue_button_y;
+
+	float button_size; //scaled button size (percentage)
+
 	sf::RenderWindow *window;
 	sf::Event event;
 
 	//Constructor
 	sfml_objects();
 
+	// Initializations
+	void init_button_size(int percentege_size);
+
 	//Displaying objects
-	void display_next_button(int pos_x, int pos_y);
+	void display_texture(int pos_x, int pos_y, std::string file_path, float scale_x, float scale_y);
+	void display_text(int pos_x, int pos_y, std::string text);
 
 	//Accessors
 	const bool getWindowIsOpen();
