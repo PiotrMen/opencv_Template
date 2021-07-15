425a72df21fb0747fd653ff054bbd37868a0f733
PiotrMen <scaryaxpl@gmail.com>
2021-07-15 08:55:30 +0200

    optymalizacja obrazu

--- opencv_Template/opencv_Template/vision.cpp
+++ opencv_Template/opencv_Template/vision.cpp
@@ -8,7 +8,11 @@ void thread_vision::operator()(int index)
 	cv::Mat image, undistorted;
 	cv::Mat CameraMatrix = (cv::Mat_<float>(3, 3) << 2257.4, 0, 624.0, 0, 2311.5, 646.2, 0, 0, 1);
 	cv::Mat DistCoeffs = (cv::Mat_<float>(1, 5) << -0.75, 0.62, 0.0015, 0.0575, -0.86);
-
+	cv::Mat map1, map2;
+	cv::Mat newCamMatrix;
+	cv::Mat Undistorted_remap;
+	camera.read(image);
+	cv::initUndistortRectifyMap(CameraMatrix, DistCoeffs, cv::Mat(), CameraMatrix, cv::Size(image.cols, image.rows), CV_32FC1, map1, map2);
 
 
 
@@ -16,13 +20,14 @@ void thread_vision::operator()(int index)
 	{
 		camera.read(image);
 
-
-		cv::undistort(image, undistorted, CameraMatrix, DistCoeffs);
+		cv::remap(image, Undistorted_remap, map1, map2, cv::INTER_CUBIC);
+		//cv::undistort(image, undistorted, CameraMatrix, DistCoeffs);
 
 
 		//imshow("main" + index, image);
 
-		imshow("main2" + index, undistorted);
+		//imshow("main2" + index, undistorted);
+		imshow("main3" + index, Undistorted_remap);
 		cv::waitKey(30);
 	}
 }
